2bcf0b62fb4e6e05f8a58703bb43c1349dfa3b02
@NISHAN <140871999+ASHADEEP-NISHAN@users.noreply.github.com>
2024-09-17 11:58:23 +0530

    First commit

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm,LoginForm,RegistrationForm,CommentForm
 import smtplib
-my_email=""
-password=""
+import os
+my_email=os.environ.get("EMAIL_KEY")
+password=os.environ.get("PASSWORD_KEY")
+
 
 def send_mail(name,email,phone,message):
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
@@ -25,7 +27,7 @@ def send_mail(name,email,phone,message):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -60,7 +62,7 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -253,4 +255,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5002)
+    app.run(debug=False)
